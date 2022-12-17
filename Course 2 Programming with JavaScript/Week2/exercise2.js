ad86857a115f690a66701214b0a439548dfd7bc7
Mohamedthameemau5 <mdthameem1993@gmail.com>
2022-12-17 14:01:55 +0530

    Exercise: Creating arrays and objects

---
+++ Course 2 Programming with JavaScript/Week2/exercise2.js
@@ -0,0 +1,14 @@
+var clothes = [];
+clothes.push("jean");  // 1st item of clothing
+clothes.push("shirt");  // 2nd item of clothing
+clothes.push("tshirt");  // 3rd item of clothing
+clothes.push("Pant");  // 4th item of clothing
+clothes.push("white shocks");  // 5th item of clothing
+clothes.pop();  // last item of clothing removed
+clothes.push("black shocks");  // 5th item of clothing added
+console.log(clothes)
+
+var favCar = {};
+favCar.color = "Red";
+favCar.convertible = true;
+console.log(favCar)
